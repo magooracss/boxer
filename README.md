dd153b3dc63e6e34207a290506bf92df0f392178
Carlos E. Rodriguez (Magoo) <lw4dbf@gmail.com>
2013-08-15 14:38:04 -0300

    Creada la estructura de la BD

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-boxer
+Boxer
 =====
 
 Seguimiento de cobros y pagos de una forma muy básica y simple. 
+
+Desarrollo
+==========
+
+El programa está realizado utilizando Lazarus 1.0.4 y las librerías ZeosLib para el acceso a la base SQLite3
+
+
+Contacto
+========
+Carlos E. Rodriguez (Magoo)
+twitter: @magooracss
+correo: lw4dbf@gmail.com
---
+++ sql/01.sql
@@ -0,0 +1,19 @@
+CREATE gutPayments
+( idPayment integer primary key
+, shortref varchar (3)
+, payment varchar(100)
+, bVisible smallint default 1 
+);
+
+CREATE TABLE items 
+( idItem integer primary key
+, itemDate	date
+, itemDescription varchar(500)
+, itemIn	float
+, itemOut	float
+, refPayment integer
+, itemClosed smallint default 0
+, itemvisible smallint default 1
+); 
+
+
